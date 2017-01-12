6710462eb7f19ef480caac32883bcb76d2711985
itvladik <vladislavbirukov@mail.ru>
2017-01-12 23:55:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
-var http  = require('http');
-var fs  = require('fs');
+var http = require('http');
+var fs = require('fs');
 var socketio = require('socket.io');
-var escape_html = require('escape-html');
+var html = require('escape-html');
 
 var server = http.createServer();
 var io = socketio(server);
-var port = 3000;
+var port = process.env.PORT || 3000;
 
-fs.readFile('./index.html', function(err, html_string){
-	if (err) {
-		throw  err;
-	}
-	io.on('connection', function(socket){
-			socket.on('message', function(data){
-				if (data && typeof data.nickname ==  'string' && typeof data.message == 'string' && data.nickname && data.message ) {}
-				//socket.broadcast.emit('message', {nickname: escape_html(data.nickname) message: escape_html(data.message)})
-			})
-	})
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }   
+    server.on('request', function(request, response) {
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.end(html);  
+    });
 
-	server.on('require', function(request, respanse){
-		respanse.writeHeader(200,{'Content-Type': 'text/html'});
-		respanse.end(html_string);
-	})
-	server.listen(port, function(){
-		console.log('Server running at port ' + port);
-	})
-})
+    server.listen(port, function() {
+      console.log('Server running at port ' + port);
+    });
+});
+
+io.on('connection', function(socket){
+  
+  socket.on('message', function(data) {
+  	if(data && typeof data.nickname == 'string' && typeof data.message == 'string' && data.nickname && data.message) {
+  		socket.broadcast.emit('message', { nickname: html(data.nickname), message: html(data.message) });
+      console.log(data);
+  	}
+  })
+
+});
